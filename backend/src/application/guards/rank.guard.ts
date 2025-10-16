02a9cb2b7d237a5797b74e93952bd19cf36b6e11
lorenzoalig <lorenzoalig@gmail.com>
2025-10-16 03:38:33 -0300

    Added self allowed condition to updating user route RankGuard

--- backend/src/application/guards/rank.guard.ts
+++ backend/src/application/guards/rank.guard.ts
@@ -9,19 +9,15 @@ export class RankGuard implements CanActivate {
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const requiredRanks = this.reflector.get(Ranks, context.getHandler());
-        console.log(requiredRanks);
 
         if(!requiredRanks) return true;
         const request = context.switchToHttp().getRequest();
-
         const isSelfAllowed = this.reflector.get(IsSelfAllowed, context.getHandler());
-        console.log(isSelfAllowed);
-
         const targetUser = request.params.id;
-        console.log(targetUser);
- 
+
+        if(isSelfAllowed && request.user.id == targetUser) return true;
         const userRank = request.user.level;
-        
+
         if(!this.matchesRanks(userRank, requiredRanks)) {
             throw new UnauthorizedException("Error: insufficient access level for this resource.");
         }
