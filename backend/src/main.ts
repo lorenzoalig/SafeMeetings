d726496fe381f21cebb2b37b329eb193b10ec58a
lorenzoalig <lorenzoalig@gmail.com>
2025-10-23 08:11:06 -0300

    Added swagger documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './application/modules/app.module';
 import { json, urlencoded } from 'express';
+import initSwagger from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  initSwagger(app);
+
   app.use(json({ limit: "10mb" }));
   app.use(urlencoded({ extended: true, limit: "10mb" }));
 
